3a51fe22f3654cba7f3f6c3ed83925a24cc147bc
MerlinPeter <merlinahila@gmail.com>
2017-08-30 01:29:18 -0700

    Jan 30 latest  Driver update.

--- Driver.java
+++ Driver.java
@@ -3,22 +3,23 @@ package automationreport;
 public class Driver {
 
 	public static void main(String[] args)  throws Exception{
-	
-		/*AutomationScripts.xeroLogin(30,32);
-		AutomationScripts.xeroLogin(35,37);
-		AutomationScripts.xeroLogin(40,42);*/
+	/* tests*/
+	//	AutomationScripts.xeroLogin(30,32);
+		//AutomationScripts.xeroLogin(35,37);
+		//AutomationScripts.xeroLogin(40,42);
 		//AutomationScripts.xeroLogin(45,47);
 		//AutomationScripts.xeroLogin(50,58);
 		AutomationScripts.xeroLogin(61,64);
+		AutomationScripts.xeroLogin(67,69);
+		AutomationScripts.xeroLogin(71,73);
 		
-	   //AutomationScripts.SFDCLogin(2,4);
-	    //AutomationScripts.SFDCLogin(6,8);
-	 //  AutomationScripts.SFDCLogin(11,16);
-	 //  AutomationScripts.SFDCLogin(19,21);
-	//   AutomationScripts.SFDCLogin(24,27);
+	/*   AutomationScripts.SFDCLogin(2,4);
+	    AutomationScripts.SFDCLogin(6,8);
+	  AutomationScripts.SFDCLogin(11,16);
+	   AutomationScripts.SFDCLogin(19,21);
+	  AutomationScripts.SFDCLogin(24,27); */
 	   
 		
-	   
 	   
 	}
 
